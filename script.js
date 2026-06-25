a864326bc2ba4a7a5b05e809c139b0f6b884daca
driplase <85550737+driplase@users.noreply.github.com>
2026-06-25 22:25:47 +0900

    chore: ok well host update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-if (location.hostname !== "www.j0.si") {
+if (location.hostname !== "legacy.www.j0.si") {
     if (location.hostname !== "127.0.0.1" && location.hostname !== "localhost") {
-        location.hostname = "www.j0.si";
+        location.hostname = "legacy.www.j0.si";
     }
 }//
